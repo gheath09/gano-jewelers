df48028122cf1fdd99714a01594559a5935b77d1
gheath09 <heathgabriel@icloud.com>
2026-03-19 09:47:11 -0400

    Add Services component listing services offered

---
+++ app/components/Services.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services: React.FC = () => {
+    return (
+        <div>
+            <h1>Our Services</h1>
+            <ul>
+                <li>Custom Rings</li>
+                <li>Diamond Sourcing</li>
+                <li>Repairs</li>
+                <li>Rolex Sourcing with Luxury Design</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Services;
