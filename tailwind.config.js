9472b573fd0be6dbb770885f705cd4439f7e663a
gheath09 <heathgabriel@icloud.com>
2026-03-19 09:12:22 -0400

    Create tailwind.config.js with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#0B0B0B',
+        gold: '#D4AF37',
+        white: '#F8F8F8',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
